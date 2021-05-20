ecf80c969270f0bab5cc0936660d384b28c9f69e
Daria <pervushdaria@gmail.com >
2021-05-20 14:44:24 +0300

    Added some styles

--- src/components/TextFrame/TextFrame.js
+++ src/components/TextFrame/TextFrame.js
@@ -5,6 +5,9 @@ import { incrementCurrentIndex, setWrongLetter } from "../../redux/actions";
 import { Loader } from "../common/Loader/Loader";
 
 import styles from "./TextFrame.module.css";
+import classNames from "classnames/bind";
+
+let cx = classNames.bind(styles);
 
 export const TextFrame = () => {
   const { text, currentIndex, wrongIndex } = useSelector(({ text }) => text);
@@ -28,30 +31,25 @@ export const TextFrame = () => {
 
   const renderText = () => {
     return text.split("").map((char, index) => {
-      if (index === currentIndex) {
-        if (wrongIndex === currentIndex) {
-          return (
-            <span key={`${char}_${index}`} className={styles.incorrectChar}>
-              {char}
-            </span>
-          );
-        }
-        return (
-          <span key={`${char}_${index}`} className={styles.correctChar}>
-            {char}
-          </span>
-        );
-      } else {
-        return char;
-      }
+      const className = cx({
+        char: true,
+        correctChar: index === currentIndex,
+        incorrectChar: index === currentIndex && index === wrongIndex,
+        passChar: index < currentIndex,
+      });
+
+      return (
+        <span key={`${char}_${index}`} className={className}>
+          {char}
+        </span>
+      );
     });
   };
 
   return (
     <div className={styles.text}>
       <div className={styles.container}>
-        <div className={styles.background}>{renderText()}</div>
-        <div className={styles.body}>{text || <Loader />}</div>
+        <div className={styles.body}>{renderText() || <Loader />}</div>
       </div>
     </div>
   );
